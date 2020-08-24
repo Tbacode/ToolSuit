b84e83aa6c7d85367c8954fc25d5f5cf078c31ed
Tommy <xt875932826@qq.com>
2020-08-24 15:53:42 +0800

    提交分类用例

--- 工具包/color_InterfaceTest/API_test/run.py
+++ 工具包/color_InterfaceTest/API_test/run.py
@@ -7,13 +7,14 @@ import unittest
 from BSTestRunner import BSTestRunner
 from BSTestRunner import _TestResult
 import time
-from test_case.tool import Tool
-import asyncio
-import aiohttp
-import json
+# from test_case.tool import Tool
+# import asyncio
+# import aiohttp
+# import json
 
 test_dir = r"./test_case"
-report_dir = r"./reports"
+report_dir_lite = r"./reports/lite"
+report_dir_color = r"./reports/color"
 now = ""
 # TC发布群
 wechat_message_for_TC = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5aae35f3-110c-4729-b776-8ad6413127db"
@@ -34,13 +35,16 @@ is_test_mode = False
 def add_testCase(test_dir):
     # 加载测试用例
     if is_test_mode:
-        discover = unittest.defaultTestLoader.discover(
+        discover1 = unittest.defaultTestLoader.discover(
             test_dir, pattern='tapcolor_test.py')
+        discover2 = None
     else:
-        discover = unittest.defaultTestLoader.discover(
+        discover1 = unittest.defaultTestLoader.discover(
             test_dir, pattern='tapcolor_api_*.py')
+        discover2 = unittest.defaultTestLoader.discover(
+            test_dir, pattern='new_color_api_*.py')
         # discover = unittest.defaultTestLoader.discover(test_dir, pattern='tapcolor_test.py')
-    return discover
+    return discover1, discover2
 
 
 # 定义报告的文件格式
@@ -66,16 +70,16 @@ def run_testCase(report_name, discover):
     #    return runner.run(discover)
 
 
-async def send_request(url, report_dir):
-    async with aiohttp.ClientSession() as session:
-        data = aiohttp.FormData()
-        data.add_field('file_1',
-                       open(report_dir, 'rb'),
-                       filename=now + 'testReport.html',
-                       content_type='multipart/form-data')
-        async with session.post(url, data=data) as resp:
-            print(await resp.text())
-            return await resp.text()
+# async def send_request(url, report_dir):
+#     async with aiohttp.ClientSession() as session:
+#         data = aiohttp.FormData()
+#         data.add_field('file_1',
+#                        open(report_dir, 'rb'),
+#                        filename=now + 'testReport.html',
+#                        content_type='multipart/form-data')
+#         async with session.post(url, data=data) as resp:
+#             print(await resp.text())
+#             return await resp.text()
 
 
 def make_report_data(test_result: _TestResult):
@@ -94,38 +98,42 @@ def make_report_data(test_result: _TestResult):
         return report_data
 
 
-def sent_wechat(my_report, report_data, wechat_message_url, wechat_upload_url):
-    Tool.call_wechat_txt(wechat_message_url, report_data)
-    print("api_test:get media_id")
-    mytext = asyncio.run(send_request(wechat_upload_url, my_report))
-    data = json.loads(mytext)
-    my_id = data['media_id']
-    Tool.call_wechat_media(wechat_message_url, my_id)
+# def sent_wechat(my_report, report_data, wechat_message_url, wechat_upload_url):
+#     Tool.call_wechat_txt(wechat_message_url, report_data)
+#     print("api_test:get media_id")
+#     mytext = asyncio.run(send_request(wechat_upload_url, my_report))
+#     data = json.loads(mytext)
+#     my_id = data['media_id']
+#     Tool.call_wechat_media(wechat_message_url, my_id)
 
 
 if __name__ == '__main__':
     now = time.strftime("%Y_%m_%d_%H_%M")
 
     print("api_test:add_testCase")
-    my_discover = add_testCase(test_dir)
+    my_discover1, my_discover2 = add_testCase(test_dir)
 
     print("api_test:get_report_name")
-    my_report = get_report_dir(report_dir)
+    my_report1 = get_report_dir(report_dir_lite)
+    my_report2 = get_report_dir(report_dir_color)
 
     print("api_test:run_testCase")
-    test_result = run_testCase(my_report, my_discover)
-    report_data = make_report_data(test_result)
+    test_result1 = run_testCase(my_report1, my_discover1)
+    report_data = make_report_data(test_result1)
 
-    if test_result.failure_count != 0:
-        if is_test_mode:
-            sent_wechat(my_report, report_data, wechat_message_for_QA2,
-                        wechat_upload_for_QA2)
-        else:
-            sent_wechat(my_report, report_data, wechat_message_for_TC,
-                        wechat_upload_for_TC)
-    elif test_result.error_count != 0:
-        sent_wechat(my_report, report_data, wechat_message_for_QA,
-                    wechat_upload_for_QA)
+    test_result2 = run_testCase(my_report2, my_discover2)
+    report_data = make_report_data(test_result2)
+
+    # if test_result.failure_count != 0:
+    #     if is_test_mode:
+    #         sent_wechat(my_report, report_data, wechat_message_for_QA2,
+    #                     wechat_upload_for_QA2)
+    #     else:
+    #         sent_wechat(my_report, report_data, wechat_message_for_TC,
+    #                     wechat_upload_for_TC)
+    # elif test_result.error_count != 0:
+    #     sent_wechat(my_report, report_data, wechat_message_for_QA,
+    #                 wechat_upload_for_QA)
 
     # if check_result(test_result) :
 
