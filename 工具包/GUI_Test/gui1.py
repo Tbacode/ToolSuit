412bce4ad332dfb7f673d6828b0accec741f9305
Tommy <xt875932826@qq.com>
2020-12-01 14:44:56 +0800

    GUI学习代码

--- 工具包/GUI_Test/gui1.py
+++ 工具包/GUI_Test/gui1.py
@@ -2,8 +2,8 @@
 Descripttion: guitest
 Author: Tommy
 Date: 2020-10-26 16:34:19
-LastEditors: Tommy
-LastEditTime: 2020-11-04 10:02:53
+ * @LastEditors  : Tommy
+ * @LastEditTime : 2020-11-25 14:47:10
 '''
 # from tkinter import Label
 # widget = Label(None, text='hello GUI world!')
@@ -82,19 +82,90 @@ lambda函数将button的command函数映射到另一个由lambda提供的带参�
 # root.mainloop()
 
 # bound方法回调处理器
-import sys
+# import sys
+# from tkinter import *
+
+
+# class HelloClass:
+#     def __init__(self):
+#         widset = Button(None, text="exit", command=self.quit)
+#         widset.pack()
+
+#     def quit(self):
+#         print("Exit....")
+#         sys.exit()
+
+
+# HelloClass()
+# mainloop()
+
+# 可调用类对象的回调处理器
+
+
+# from tkinter import *
+# import sys
+# class HelloCallable:
+#     def __init__(self):
+#         self.msg = 'hello __call__ world'
+
+#     def __call__(self):
+#         print(self.msg)
+#         sys.exit()
+
+
+# Widget = Button(None, text='hello event world', command=HelloCallable())
+# Widget.pack()
+# Widget.mainloop()
+
+# 多组件加入
+# from tkinter import *
+
+# def greeting():
+#     print('Hello stdout world!..')
+
+# win = Frame()
+# win.pack()
+# Label(win, text='Hello container world').pack(side=TOP)
+# Button(win, text='Hello', command=greeting).pack(side=LEFT)
+# Label(win, text='Hello container world').pack(side=TOP)
+# Button(win, text='Quit', command=win.quit).pack(side=RIGHT)
+# Label(win, text='Hello container world').pack(side=TOP)
+'''
+    TIP：先打包的组件总是最后消失，
+    eg：尽管Label的布局位于top，但是窗口缩小时，只有打包顺序会影响布局
+'''
+
+# win.mainloop()
+
+# packer的expand和fill选项
+# from tkinter import *
+
+# def greeting():
+#     print("hello world")
+
+# win = Frame()
+# win.pack(side=TOP, expand=YES, fill=BOTH)
+# Button(win, text='Hello', command=greeting).pack(side=LEFT, SSSfill=Y)
+# Label(win, text='Hello container world').pack(side=TOP)
+# Button(win, text='Quit', command=win.quit).pack(side=RIGHT, expand=YES, fill=X)
+
+# win.mainloop()
+
+
+
+
 from tkinter import *
+class ThemeButton(Button):
+    """
+    自定义button类
+    """
 
+    def __init__(self, parent=None, **configs):
+        Button.__init__(self, parent, **configs)
+        self.pack()
+        self.config(fg='red', bg='black', font=(
+            'courier', 12), relief=RAISED, bd=5)
 
-class HelloClass:
-    def __init__(self):
-        widset = Button(None, text="exit", command=self.quit)
-        widset.pack()
-
-    def quit(self):
-        print("Exit....")
-        sys.exit()
-
-
-HelloClass()
-mainloop()
+B1 = ThemeButton(text='spam', command=onSpam)
+B2 = ThemeButton(text='eggs')
+B2.pack(expand=YES, fill=BOTH)
