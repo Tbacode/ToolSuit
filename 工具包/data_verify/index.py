e49512295ae637bfb1e21668541621bab5ed30af
PC-20190310LARV\Administrator <xt875932826@126.com>
2020-12-13 23:20:15 +0800

    提交完善主方法入口

--- 工具包/data_verify/index.py
+++ 工具包/data_verify/index.py
@@ -3,7 +3,7 @@
  * @Author       : Tommy
  * @Date         : 2020-12-07 15:45:54
  * @LastEditors  : Tommy
- * @LastEditTime : 2020-12-11 00:45:28
+ * @LastEditTime : 2020-12-13 23:13:30
 '''
 from data_get import DataGet
 from pic_verify import PicVerify
@@ -18,6 +18,29 @@ def main(data_base: DataGet):
         logger.debug("此时的数据长度：" + str(len(data_base.pic_list_item)))
         logger.debug("开始新一轮递归")
         main(data_base)
+
+
+def pre_main(url: str, game_ver: str, os_type: str):
+    # 这里验证数据的初始化
+    pictype_list = [
+        "Jigsaw", "Cartoon", "Fashion", "Food", "Marine", "Festival",
+        "Unicorn", "Love", "Christmas", "Animated", "Special",
+        "Character", "Animal", "Flower", "Place", "Nature",
+        "Message", "Mosaic", "Mandala", "Other"
+    ]
+    for pic_type in pictype_list:
+        logger.debug("请求开始类型：{}".format(pic_type))
+        data_base = DataGet(url, game_ver, os_type, pic_type)
+        pic_verify = PicVerify()
+        result = data_base.request_pic_list_item()
+        if pic_verify.is_empty_by_pic_type(result['data']['picList']):
+            main(data_base)
+            if pic_type == 'Jigsaw':
+                pic_verify.is_error_by_jigsawpic(data_base.pic_list_item)
+            logger.debug("最后的数据：{}".format(
+                data_base.pic_list_item[-1]['picName']))
+        else:
+            logger.error("存在分类{}数据为空".format(pic_type))
 
 
 def start(env_name: str, obj_name: str, os_type: str):
@@ -35,31 +58,8 @@ def start(env_name: str, obj_name: str, os_type: str):
                     url = ''.join([base_url, api_name])
                     game_ver = con_json[key][obj_item]['game_ver']
                     os_type = os_type
-                    print(url)
+                    pre_main(url, game_ver, os_type)
 
 
 if __name__ == "__main__":
-    # base_url = "https://tapcolor.weplayer.cc/"
-    # url = ''.join([base_url, 'normalApi/v1/getGalleryList'])
-    # game_ver = "4.7.0"
-    # os_type = "Android"
-    # pictype_list = [
-    #     "Jigsaw", "Cartoon", "Fashion", "Food", "Marine", "Festival",
-    #     "Unicorn", "Love", "Christmas", "Animated", "Special",
-    #     "Character", "Animal", "Flower", "Place", "Nature",
-    #     "Message", "Mosaic", "Mandala", "Other"
-    # ]
-    # for pic_type in pictype_list:
-    #     logger.debug("请求开始类型：{}".format(pic_type))
-    #     data_base = DataGet(url, game_ver, os_type, pic_type)
-    #     pic_verify = PicVerify()
-    #     result = data_base.request_pic_list_item()
-    #     if pic_verify.is_empty_by_pic_type(result['data']['picList']):
-    #         main(data_base)
-    #         if pic_type == 'Jigsaw':
-    #             pic_verify.is_error_by_jigsawpic(data_base.pic_list_item)
-    #         logger.debug("最后的数据：{}".format(
-    #             data_base.pic_list_item[-1]['picName']))
-    #     else:
-    #         logger.error("存在分类{}数据为空".format(pic_type))
-    start("pre", "TapColor", "Android")
+    start("release", "TapColor", "Android")
