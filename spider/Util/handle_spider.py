3ebb625eeb9c8cb72b9f87992e51b43bccb99fc6
Tommy <xt875932826@qq.com>
2021-05-18 18:49:29 +0800

    调教修改

--- spider/Util/handle_spider.py
+++ spider/Util/handle_spider.py
@@ -3,19 +3,23 @@
  * @Author       : Tommy
  * @Date         : 2021-05-12 17:17:39
  * @LastEditors  : Tommy
- * @LastEditTime : 2021-05-12 17:38:44
+ * @LastEditTime : 2021-05-18 11:27:32
 '''
 
 
 class HandleSpider(object):
-    def __init__(self):
+    def __init__(self, url):
         '''
          * @name: Tommy
          * @msg: HandleExcel 初始化
-         * @param {*}
+         * @param {初始化url,headers}
          * @return {*}
         '''
-        pass
+        self.url = url
+        self.headers = {
+            'User-Agent':
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)'
+        }
 
     def get_html_by_bs4(self):
         '''
