028b1a5c9a3f0df668d1a7542b80668ff0100779
Tommy <xt875932826@qq.com>
2021-05-21 19:01:53 +0800

    提交

--- spider/main.py
+++ spider/main.py
@@ -3,10 +3,39 @@
  * @Author       : Tommy
  * @Date         : 2021-05-12 17:16:44
  * @LastEditors  : Tommy
- * @LastEditTime : 2021-05-20 15:49:57
+ * @LastEditTime : 2021-05-21 16:39:06
 '''
 # from Util.handle_excel import HandleExcel
 from Util.handle_spider import HandleSpider
+
+
+def url_in_list(url, handleobj, xpath, hrefxpath):
+    '''
+     * @name: Tommy
+     * @msg: 根据xpath获取页面内所有子链接，返回一个链接list
+     * @param {url:页面地址;handleobj:handleSpider实例化对象;xpath:目标元素xpath;hrefxpath:链接xpath}
+     * @return {url子链接集合}
+    '''
+    html = handleobj.get_html(url)
+    html_list = handleobj.get_elements_by_xpath(html, xpath)
+    list_return = []
+    for index, item in enumerate(html_list):
+        href = handleobj.get_element_value_by_xpath(item, hrefxpath.format(index + 1))
+        list_return.append(href[0])
+    return list_return
+
+
+def url_combination(homeUrl, linkUrl_list):
+    '''
+     * @name: Tommy
+     * @msg: 组装主页面链接和子链接形成完整的链接集合返回
+     * @param {homeUrl:主页链接; linkUrl_list:子链接集合}
+     * @return {url链接集合}
+    '''
+    url_list_return = []
+    for link_item in linkUrl_list:
+        url_list_return.append(homeUrl + link_item)
+    return url_list_return
 
 
 def main(url):
