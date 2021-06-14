b3b6c107372cb7edaafa2f2660f43ea9589f80ab
徐拓家 <xt875932826@126.com>
2021-06-14 18:10:15 +0800

    新建文本

---
+++ selenium_project/index.py
@@ -0,0 +1,7 @@
+'''
+ * @Description  : 
+ * @Autor        : Tommy
+ * @Date         : 2021-06-14 18:05:18
+ * @LastEditors  : Tommy
+ * @LastEditTime : 2021-06-14 18:05:18
+'''
---
+++ selenium_project/test/test.py
@@ -0,0 +1,11 @@
+'''
+ * @Description  : 
+ * @Autor        : Tommy
+ * @Date         : 2021-06-14 18:03:05
+ * @LastEditors  : Tommy
+ * @LastEditTime : 2021-06-14 18:04:04
+'''
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://www.baidu.com')
