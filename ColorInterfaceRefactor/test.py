2b4376ce4b81bfd468ebd60fbb0f3f08bff9c815
Tommy <xt875932826@qq.com>
2021-06-16 16:49:14 +0800

    笔记提交。

---
+++ ColorInterfaceRefactor/test.py
@@ -0,0 +1,7 @@
+'''
+ * @Descripttion : 
+ * @Author       : Tommy
+ * @Date         : 2021-06-16 16:48:28
+ * @LastEditors  : Tommy
+ * @LastEditTime : 2021-06-16 16:48:29
+'''
--- selenium_project/test/test.py
+++ selenium_project/test/test.py
@@ -3,9 +3,10 @@
  * @Autor        : Tommy
  * @Date         : 2021-06-14 18:03:05
  * @LastEditors  : Tommy
- * @LastEditTime : 2021-06-14 18:04:04
+ * @LastEditTime : 2021-06-16 11:47:54
 '''
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
+driver.find_element_by_id('kw')
